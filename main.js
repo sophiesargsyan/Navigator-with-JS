cf7b32dc1714d424e5d252b76bc078f101889b5d
Sophie Sargsyan <144236558+sophiesargsyan@users.noreply.github.com>
2024-04-24 16:08:32 +0400

    Removed prompting for coordinates and navigation method

--- main.js
+++ main.js
@@ -59,28 +59,6 @@ class BicycleNavigator extends Navigator {
     }
 }
 
-function promptForCoordinates() {
-    const latitude = parseFloat(prompt("Enter latitude:"));
-    const longitude = parseFloat(prompt("Enter longitude:"));
-    return { latitude, longitude };
-}
-
-function promptForNavigationMethod() {
-    const method = prompt("Choose navigation method: 'walk', 'car', or 'bike'");
-    return method.toLowerCase();
-}
-
-const currentCoordinates = promptForCoordinates();
-console.log("Current Coordinates:", currentCoordinates);
-
-const destinationTime = parseFloat(prompt("Enter destination time in hours:"));
-console.log("Destination Time:", destinationTime);
-
-const destinationCoordinates = promptForCoordinates();
-console.log("Destination Coordinates:", destinationCoordinates);
-
-const navigationMethod = promptForNavigationMethod();
-
 let navigator;
 switch (navigationMethod) {
     case 'walk':
